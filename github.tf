f2ff4d2ed0e952f627bae5b81c71bf7bb97fd43d
Artem Bielskyi <abiels@softserveinc.com>
2022-01-12 10:20:38 +0200

    Create infrastructure repo. Add collaborators

--- github.tf
+++ github.tf
@@ -18,9 +18,42 @@ resource "github_repository" "application" {
   visibility  = "private"
 }
 
-resource "github_repository_collaborator" "application_belskiiartem" {
+resource "github_repository" "infrastructure" {
+  name        = "infrastructure"
+  visibility  = "private"
+}
+
+
+resource "github_repository_collaborator" "infrastructure_Serhii-Bahlai" {
+  repository = "infrastructure"
+  username   = "Serhii-Bahlai"
+  permission = "admin"
+  depends_on = [
+    github_repository.infrastructure,
+  ]
+}
+
+resource "github_repository_collaborator" "application_Serhii-Bahlai" {
   repository = "application"
-  username   = "belskiiartem"
+  username   = "Serhii-Bahlai"
+  permission = "admin"
+  depends_on = [
+    github_repository.application,
+  ]
+}
+
+resource "github_repository_collaborator" "infrastructure_RPalaziuk" {
+  repository = "infrastructure"
+  username   = "RPalaziuk"
+  permission = "admin"
+  depends_on = [
+    github_repository.infrastructure,
+  ]
+}
+
+resource "github_repository_collaborator" "application_RPalaziuk" {
+  repository = "application"
+  username   = "RPalaziuk"
   permission = "admin"
   depends_on = [
     github_repository.application,
