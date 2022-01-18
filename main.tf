46e2452f7e99e72a3202144bd80f2e1aaa927b3f
Artem Bielskyi <abiels@softserveinc.com>
2022-01-18 11:11:07 +0200

    use storage account as terraform backend

--- main.tf
+++ main.tf
@@ -1,10 +1,16 @@
 terraform {
-required_providers {
-    azurerm = {
-    source  = "hashicorp/azurerm"
-    version = "=2.92.0"
+    required_providers {
+        azurerm = {
+        source  = "hashicorp/azurerm"
+        version = "=2.92.0"
+        }
     }
-}
+    backend "azurerm" {
+        resource_group_name  = "abiels_tfstate"
+        storage_account_name = "tfstateubc1m"
+        container_name       = "tfstate"
+        key                  = "terraform.tfstate"
+    }
 }
 
 # Configure the Microsoft Azure Provider
