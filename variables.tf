e470979e736ad7e409ab26440569f07ebc2e434a
Artem Bielskyi <abiels@softserveinc.com>
2022-01-20 16:04:22 +0200

    remove unused variable

--- variables.tf
+++ variables.tf
@@ -12,10 +12,6 @@ variable "allowed_ips" {
     abiels = "109.251.247.244/32"
   }
 }
-variable "rg_id" {
-  type = string
-  default = "001"
-}
 variable resource_group_name {
   type = string
   default = "abiels-dev-rg-001"
